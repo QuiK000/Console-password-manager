c1b0b97edfbccdfd0d647353607f82ea45fcc9fe
QuiK000 <teamquiksteam@gmail.com>
2026-05-02 18:39:14 +0300

    Wire application dependencies in Main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,19 @@
 import cli.CommandHandler;
 import cli.ConsoleUI;
+import cli.commands.AddCommand;
+import cli.commands.ListCommand;
+import model.Vault;
+import service.impl.VaultServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
-        CommandHandler commandHandler = new CommandHandler();
+        Vault vault = new Vault();
+        VaultServiceImpl vaultService = new VaultServiceImpl(vault);
+
+        AddCommand addCommand = new AddCommand(vaultService);
+        ListCommand listCommand = new ListCommand(vaultService);
+
+        CommandHandler commandHandler = new CommandHandler(addCommand, listCommand);
         ConsoleUI console = new ConsoleUI(commandHandler);
 
         console.run();
