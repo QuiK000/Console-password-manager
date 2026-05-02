e02fe867017a4f3a379d809617fefdfbaee0b653
QuiK000 <teamquiksteam@gmail.com>
2026-05-02 18:36:47 +0300

    Enhance Entry with Lombok annotations and Vault with business methods

--- src/main/java/model/Entry.java
+++ src/main/java/model/Entry.java
@@ -1,7 +1,14 @@
 package model;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
 import java.time.LocalDateTime;
 
+@Builder
+@Getter
+@ToString
 public class Entry {
     private String id;
     private String site;
--- src/main/java/model/Vault.java
+++ src/main/java/model/Vault.java
@@ -1,7 +1,16 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Vault {
-    private List<Entry> entries;
+    private final List<Entry> entries = new ArrayList<>();
+
+    public void addEntry(Entry entry) {
+        this.entries.add(entry);
+    }
+
+    public List<Entry> getEntries() {
+        return entries.stream().toList();
+    }
 }
