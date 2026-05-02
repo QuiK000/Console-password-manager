5ef6dd22c804fe8542177d2c32abb781927795c7
QuiK000 <teamquiksteam@gmail.com>
2026-05-02 19:21:27 +0300

    Implement DeleteCommand and GetCommand with vault service integration

--- src/main/java/cli/commands/DeleteCommand.java
+++ src/main/java/cli/commands/DeleteCommand.java
@@ -1,4 +1,14 @@
 package cli.commands;
 
+import lombok.AllArgsConstructor;
+import model.Entry;
+import service.IVaultService;
+
+@AllArgsConstructor
 public class DeleteCommand {
+    private final IVaultService vaultService;
+
+    public void removeEntry(Entry entry) {
+        vaultService.deleteEntry(entry);
+    }
 }
--- src/main/java/cli/commands/GetCommand.java
+++ src/main/java/cli/commands/GetCommand.java
@@ -1,4 +1,14 @@
 package cli.commands;
 
+import lombok.AllArgsConstructor;
+import model.Entry;
+import service.IVaultService;
+
+@AllArgsConstructor
 public class GetCommand {
+    private final IVaultService vaultService;
+
+    public Entry getEntry(Entry entry) {
+        return vaultService.getEntry(entry);
+    }
 }
