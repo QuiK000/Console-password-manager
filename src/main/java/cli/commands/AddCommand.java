151e4b10d2be85bdedad0326178f449fca1fe455
QuiK000 <teamquiksteam@gmail.com>
2026-05-02 18:38:10 +0300

    Implement AddCommand and ListCommand with vault service integration

--- src/main/java/cli/commands/AddCommand.java
+++ src/main/java/cli/commands/AddCommand.java
@@ -1,4 +1,14 @@
 package cli.commands;
 
+import lombok.AllArgsConstructor;
+import model.Entry;
+import service.IVaultService;
+
+@AllArgsConstructor
 public class AddCommand {
+    private final IVaultService vaultService;
+
+    public void addEntry(Entry entry) {
+        vaultService.addEntry(entry);
+    }
 }
--- src/main/java/cli/commands/ListCommand.java
+++ src/main/java/cli/commands/ListCommand.java
@@ -1,4 +1,16 @@
 package cli.commands;
 
+import lombok.AllArgsConstructor;
+import model.Entry;
+import service.IVaultService;
+
+import java.util.List;
+
+@AllArgsConstructor
 public class ListCommand {
+    private final IVaultService vaultService;
+
+    public List<Entry> getEntries() {
+        return vaultService.listEntries();
+    }
 }
