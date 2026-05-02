11525561f9866dc7bf69596da3f0559ee150aae9
QuiK000 <teamquiksteam@gmail.com>
2026-05-02 18:37:38 +0300

    Implement VaultServiceImpl with Vault delegation

--- src/main/java/service/impl/VaultServiceImpl.java
+++ src/main/java/service/impl/VaultServiceImpl.java
@@ -1,23 +1,28 @@
 package service.impl;
 
+import lombok.AllArgsConstructor;
 import model.Entry;
+import model.Vault;
 import service.IVaultService;
 
 import java.util.List;
 
+@AllArgsConstructor
 public class VaultServiceImpl implements IVaultService {
+    private final Vault vault;
+
     @Override
     public void addEntry(Entry entry) {
-
+        vault.addEntry(entry);
     }
 
     @Override
     public List<Entry> listEntries() {
-        return List.of();
+        return vault.getEntries();
     }
 
     @Override
-    public Entry getEnty(String id) {
+    public Entry getEntry(String id) {
         return null;
     }
 
