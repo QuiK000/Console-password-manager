694dff40847616f96b023a5c4ce79e8feb8acce0
QuiK000 <teamquiksteam@gmail.com>
2026-05-05 20:08:39 +0300

    Add encryption support to VaultServiceImpl

--- src/main/java/service/impl/VaultServiceImpl.java
+++ src/main/java/service/impl/VaultServiceImpl.java
@@ -1,33 +1,57 @@
 package service.impl;
 
-import lombok.AllArgsConstructor;
+import crypto.ICryptoService;
 import model.Entry;
 import model.Vault;
 import service.IVaultService;
 import storage.IStorageService;
 import storage.IVaultSerializer;
 
-import java.io.IOException;
+import javax.crypto.SecretKey;
+import java.util.Arrays;
 import java.util.List;
 
-@AllArgsConstructor
 public class VaultServiceImpl implements IVaultService {
     private Vault vault;
 
     private final IStorageService storage;
     private final IVaultSerializer serializer;
+    private final ICryptoService crypto;
+
+    private SecretKey key;
+    private byte[] salt;
+
+    private static final int SALT_LENGTH = 16;
+
+    public VaultServiceImpl(IStorageService storage, IVaultSerializer serializer, ICryptoService crypto) {
+        this.storage = storage;
+        this.serializer = serializer;
+        this.crypto = crypto;
+    }
+
+    public void setSecurity(SecretKey key, byte[] salt) {
+        this.key = key;
+        this.salt = salt;
+    }
 
     @Override
     public void init() {
         try {
             if (storage.exists()) {
                 byte[] data = storage.load();
-                vault = serializer.deserialize(data);
+                salt = Arrays.copyOfRange(data, 0, SALT_LENGTH);
+
+                byte[] encrypted = Arrays.copyOfRange(data, SALT_LENGTH, data.length);
+                byte[] decrypted = crypto.decrypt(encrypted, key);
+
+                vault = serializer.deserialize(decrypted);
+                System.out.println("Vault unlocked");
             } else {
                 vault = new Vault();
+                System.out.println("New vault created");
             }
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to load vault", e);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to init value (wrong password or corrupted file)", e);
         }
     }
 
@@ -55,9 +79,15 @@ public class VaultServiceImpl implements IVaultService {
 
     private void save() {
         try {
-            byte[] data = serializer.serialize(vault);
-            storage.save(data);
-        } catch (IOException e) {
+            byte[] serialized = serializer.serialize(vault);
+            byte[] encrypted = crypto.encrypt(serialized, key);
+            byte[] finalData = new byte[salt.length + encrypted.length];
+
+            System.arraycopy(salt, 0, finalData, 0, salt.length);
+            System.arraycopy(encrypted, 0, finalData, salt.length, encrypted.length);
+
+            storage.save(finalData);
+        } catch (Exception e) {
             throw new RuntimeException("Failed to save vault", e);
         }
     }
