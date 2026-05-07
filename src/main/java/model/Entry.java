00bad319c50500cc5744c832f5ed7b1f18f7814f
QuiK000 <teamquiksteam@gmail.com>
2026-05-07 22:43:23 +0300

    Add @EqualsAndHashCode annotation to Entry based on id field

--- src/main/java/model/Entry.java
+++ src/main/java/model/Entry.java
@@ -2,6 +2,7 @@ package model;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Entry {
     private String id;
     private String site;
