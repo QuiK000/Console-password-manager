ff010228d50525282e37c0ab4f2db6d5b756667a
QuiK000 <teamquiksteam@gmail.com>
2026-05-12 19:08:36 +0300

    Enhance PasswordGenerator with configurable character sets

--- src/main/java/util/PasswordGenerator.java
+++ src/main/java/util/PasswordGenerator.java
@@ -3,13 +3,23 @@ package util;
 import java.security.SecureRandom;
 
 public class PasswordGenerator {
-    private static final String CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+    public static final String UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static final String LOWER = "abcdefghijklmnopqrstuvwxyz";
+    public static final String DIGITS = "0123456789";
+    public static final String SYMBOLS = "!@#$%^&*()-_=+";
+
     private static final SecureRandom RANDOM = new SecureRandom();
 
-    public static char[] generate(int length) {
-        char[] password = new char[length];
+    public static char[] generate(int length, boolean useUpper, boolean useDigits, boolean useSymbols) {
+        StringBuilder charset = new StringBuilder(LOWER);
 
-        for (int i = 0; i < length; i++) password[i] = CHARS.charAt(RANDOM.nextInt(CHARS.length()));
+        if (useUpper) charset.append(UPPER);
+        if (useDigits) charset.append(DIGITS);
+        if (useSymbols) charset.append(SYMBOLS);
+
+        char[] password = new char[length];
+        for (int i = 0; i < length; i++) password[i] = charset.charAt(RANDOM.nextInt(charset.length()));
+
         return password;
     }
 }
